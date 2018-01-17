aa162ab4242832dc9a5a4a01caa5a0b12605c1de
as-easy <varankinva@yandex.ru>
2018-01-17 14:28:27 +0300

    1_lesson

---
+++ 1_lesson/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1_lesson
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            Game.Init(form);
+            form.Show();
+            Game.Draw();
+            Application.Run(form);
+        }
+    }
+}
