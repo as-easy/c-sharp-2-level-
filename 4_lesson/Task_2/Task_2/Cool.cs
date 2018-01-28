d7a1555cbb8592a6b3dfc7414b94c4aae4cdcb5b
as-easy <varankinva@yandex.ru>
2018-01-29 02:39:18 +0300

    2 task

---
+++ 4_lesson/Task_2/Task_2/Cool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Cool<T>
+    {
+        public void Fun(List<T> coll)
+        {
+            int n = 1;
+
+            coll.Sort();
+
+            for (int i = 0; i < coll.Count; i++)
+            {
+                Console.Write($"Object {coll[i]}");
+                for (int j = i+1; j < coll.Count; j++)
+                {
+
+                    if (coll[i].Equals(coll[j])) n++;
+                    else 
+                    {
+                        i = j-1;
+                        break;
+                    }
+
+                    if (j == coll.Count - 1) i = j;
+                }
+
+                Console.Write($" occurs in the collection: {n} times");
+                n = 1;
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 4_lesson/Task_2/Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<int> arrListInt = new List<int>
+            {
+               // 87,1,1,2,2,3,6,2,4,2,7,4,9,2,893,4,5,87,2,4,52,893,7,23,5,6,
+               1,1,2,3,4,5,1,2,3,4,5,5,5
+            };
+            Cool<int> displayInt = new Cool<int>();
+            displayInt.Fun(arrListInt);
+            Console.WriteLine();
+
+            List<string> arrListString = new List<string>
+            {
+                "aaaa","aaaa", "aaaa", "bb","bb", "aaaa","c"
+            };    
+            Cool<string> displayString = new Cool<string>();
+            displayString.Fun(arrListString);
+
+            var elements = from n in arrListInt //c LINQ пока не сделал
+                          orderby n descending                           
+                          select n;            
+
+            Console.ReadKey();
+
+        }        
+    }
+}
