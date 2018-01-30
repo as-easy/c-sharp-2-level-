ac0145efb8a5458782b10d454860307876edaa44
as-easy <varankinva@yandex.ru>
2018-01-30 21:35:27 +0300

    5_lesson

--- 4_lesson/Task_2/Task_2/Program.cs
+++ 4_lesson/Task_2/Task_2/Program.cs
@@ -11,7 +11,7 @@ namespace Task_2
         static void Main(string[] args)
         {
 
-
+            #region Сравнение целых чисел
             List<int> arrListInt = new List<int>
             {
                // 87,1,1,2,2,3,6,2,4,2,7,4,9,2,893,4,5,87,2,4,52,893,7,23,5,6,
@@ -20,17 +20,35 @@ namespace Task_2
             Cool<int> displayInt = new Cool<int>();
             displayInt.Fun(arrListInt);
             Console.WriteLine();
+            #endregion
 
+            #region Сравнение строк
             List<string> arrListString = new List<string>
             {
                 "aaaa","aaaa", "aaaa", "bb","bb", "aaaa","c"
             };    
             Cool<string> displayString = new Cool<string>();
             displayString.Fun(arrListString);
+            #endregion            
+
+            List<int> list3 = new List<int>();
+
+            foreach (var item1 in arrListInt)
+            {
+                if (arrListInt.Where(item2 => item2 == item1).Count() == 0)
+                {
+                    list3.Add(item1);
+                }
+            }
 
             var elements = from n in arrListInt //c LINQ пока не сделал
-                          orderby n descending                           
-                          select n;            
+                           orderby n descending
+                           select n;
+
+            foreach (var el in elements)
+            {
+                Console.WriteLine(el);
+            }
 
             Console.ReadKey();
 
