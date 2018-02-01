9bddf5765631e979f1574ace258474d27328b07b
as-easy <varankinva@yandex.ru>
2018-02-01 15:09:48 +0300

    5_lesson

--- 5_lesson/MainWindow.xaml.cs
+++ 5_lesson/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
 
+
 namespace _5_lesson
 {
     /// <summary>
@@ -21,31 +22,45 @@ namespace _5_lesson
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<Employee> items = new ObservableCollection<Employee>();
+        ObservableCollection<Employee> itemsEmp = new ObservableCollection<Employee>();
+        ObservableCollection<Departmen> itemsDep = new ObservableCollection<Departmen>();
         public MainWindow()
         {
             InitializeComponent();
             FillList();
         }
         void FillList()
-        {
-            items.Add(new Employee() { Id = 1, Name = "Vasya", Age = 22, Salary = 3000 });
-            items.Add(new Employee() { Id = 2, Name = "Petya", Age = 25, Salary = 6000 });
-            items.Add(new Employee() { Id = 3, Name = "Kolya", Age = 23, Salary = 8000 });
-            lbEmployee.ItemsSource = items;
+        {            
+            itemsEmp.Add(new Employee() { Id = 1, Name = "Vasya", Age = 22, Salary = 3000 });
+            itemsEmp.Add(new Employee() { Id = 2, Name = "Petya", Age = 25, Salary = 6000 });
+            itemsEmp.Add(new Employee() { Id = 3, Name = "Kolya", Age = 23, Salary = 8000 });
+            lbEmployee.ItemsSource = itemsEmp;
+
+            itemsDep.Add(new Departmen() { Id = 1, Name = "IT" });
+            itemsDep.Add(new Departmen() { Id = 2, Name = "NotIT" });
+            lbDepartmen.ItemsSource = itemsDep;
         }
-        private void lbEmployee_Selected(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(e.Source.ToString());
-        }
+        //private void lbEmployee_Selected(object sender, RoutedEventArgs e)
+        //{
+        //    //MessageBox.Show(e.Source.ToString());
+        //}
         private void lbEmployee_SelectionChanged(object sender,
         System.Windows.Controls.SelectionChangedEventArgs e)
         {
             MessageBox.Show(e.AddedItems[0].ToString());
         }
+        private void lbDepartmen_SelectionChanged(object sender,
+        System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+        //private void lbDelbDepartmen_SelectionChanged(
+        //{
+        //    
+        //}
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            items.Add(new Employee() { Id = 1, Name = "Sergey", Age = 26, Salary = 7000 });
+            itemsEmp.Add(new Employee() { Id = 1, Name = "Sergey", Age = 26, Salary = 7000 });
         }
     }
     public class Employee
@@ -57,6 +72,16 @@ namespace _5_lesson
         public override string ToString()
         {
             return $"{Id}\t{Name}\t{Age}\t{Salary}";
+        }
+    }
+
+    public class Departmen 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return $"{Id}\t{Name}";
         }
     }
 
